92fbdebf45ea089d93ef8983f37f1714f172b447
aml <devaml2001@gmail.com>
2025-03-12 16:38:35 +0200

    upload files

---
+++ app/Components/Contact.jsx
@@ -0,0 +1,33 @@
+"use client";
+
+import React from "react";
+import {
+  FeedbackContainer,
+  FormContainer,
+  FeedbackTitle,
+  InputField,
+  TextArea,
+  SubmitButton,
+  ImageContainer,
+} from "../styledComponent/CustomerRatings/styledCustomerRatings";
+
+// contact component
+const Contact = () => {
+  return (
+    <FeedbackContainer>
+      {/* الصورة الجانبية */}
+      <ImageContainer />
+
+      {/* النموذج */}
+      <FormContainer>
+        <FeedbackTitle>تواصل معنا</FeedbackTitle>
+        <InputField type="text" placeholder="الاسم الثلاثي" />
+        <InputField type="email" placeholder="بريدك الإلكتروني" />
+        <TextArea placeholder="تفاصيل . . ." />
+        <SubmitButton>إرسال</SubmitButton>
+      </FormContainer>
+    </FeedbackContainer>
+  );
+};
+
+export default Contact;
